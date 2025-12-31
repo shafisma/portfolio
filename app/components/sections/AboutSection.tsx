4448168e31066820f0fe933c34470e02678f9035
shafisma <uzzamanshafi511@gmail.com>
2025-12-31 21:15:20 +0600

    Fix grid issue

--- app/components/sections/AboutSection.tsx
+++ app/components/sections/AboutSection.tsx
@@ -58,7 +58,7 @@ export function AboutSection() {
         </div>
 
         {/* Cards grid */}
-        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {aboutCards.map((card, index) => (
             <div
               key={card.title}
