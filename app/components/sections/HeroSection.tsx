c660cb06335166c6252c0347670b51941ddafc7a
shafisma <uzzamanshafi511@gmail.com>
2025-12-31 13:49:13 +0600

    Remove scroll indicator

--- app/components/sections/HeroSection.tsx
+++ app/components/sections/HeroSection.tsx
@@ -97,12 +97,6 @@ export function HeroSection() {
           </a>
         </div>
 
-        {/* Scroll indicator */}
-        <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
-          <svg className="w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-          </svg>
-        </div>
       </div>
     </section>
   );
