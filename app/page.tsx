057ab0cc250323ee77ae0c027469b2a022305adc
Shafiuzzaman <uzzamanshafi511@gmail.com>
2026-01-10 18:29:19 +0600

    refactor: remove unused state management from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { Navbar, Footer } from "./components/layout";
 import {
   HeroSection,
@@ -11,11 +10,9 @@ import {
 } from "./components/sections";
 
 export default function Home() {
-  const [activeSection, setActiveSection] = useState("about");
-
   return (
     <div className="min-h-screen text-foreground font-[family-name:var(--font-geist-sans)]">
-      <Navbar activeSection={activeSection} setActiveSection={setActiveSection} />
+      <Navbar />
       <main className="relative z-10 p-4 sm:p-8 space-y-24 container mx-auto">
         <HeroSection />
         <AboutSection />
